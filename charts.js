282cc2a1adc876ca40d0e92fbf7e8490dc765326
CJ Cheng <cjcheng22@yahoo.com>
2022-08-20 18:29:05 -0700

    After Deliverable2

--- charts.js
+++ charts.js
@@ -93,8 +93,7 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
-  });
-
+  
     // 1. Create the trace for the bubble chart.
     var bubbleData = [
       {
@@ -122,4 +121,5 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
-  }
+  });
+}
